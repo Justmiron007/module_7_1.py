1200ace0b373a74c46e04034af9e4a152c488080
Justmiron007 <raen.96@mail.ru>
2024-07-27 10:37:40 +0500

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -8,36 +8,51 @@ class Product:
         self.category = category
 
     def __str__(self):
-        return (f'{self.name}, {self.weight}, {self.category}')
+        return f'{self.name}, {self.weight}, {self.category}'
 
 
-class Shop():
+class Shop:
     __file_name = 'products.txt'
 
     def get_products(self):
-        file = open('products.txt', 'r')
-        pprint(file.read())
+        try:
+            file = open(self.__file_name, 'r')
+        except FileNotFoundError:
+            print("Файл не существует")
+            return ""
+
+        products = file.read()
         file.close()
+        return products
 
     def add(self, *products):
-        for i in products:
-            s = (str(i))
-            file = open('products.txt', 'w')
-            f = file.write()
+        existing_product_names = set()
+        try:
+            file = open(self.__file_name, 'r')
+            for line in file:
+                name = line.split(',')[0].strip()
+                existing_product_names.add(name)
             file.close()
-            if s in f:
-                print(f'Продукт {s} уже есть в магазине')
+        except FileNotFoundError:
+            print("Файл не существует")
+
+        file = open(self.__file_name, 'a')
+        for product in products:
+            if product.name in existing_product_names:
+                print(
+                    f'Продукт {product.name} уже есть в магазине')
             else:
-                file = open('products.txt', 'a')
-                file.write(f'\n{s}')
-                file.close()
+                file.write(f'{product}\n')
+                existing_product_names.add(product.name)
+        file.close()
+
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2)  # __str__
+print(p2)
 
 s1.add(p1, p2, p3)
 
